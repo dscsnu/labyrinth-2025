7b0ce026adec224df630cd54b97fb7f5550ce5f9
Eklavya Nath <optimusprime172004@gmail.com>
2025-04-11 00:48:42 +0530

    eklavya- labrynth  setup

---
+++ frontend/src/routes/team/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    let name = 'world';
+  </script>
+  
+  <main>
+    <h1>Hello {name}!</h1>
+    <input bind:value={name} placeholder="Enter your name" />
+  </main>
+  
+  <style>
+    main {
+      padding: 2rem;
+      font-family: sans-serif;
+      text-align: center;
+    }
+  </style>
+  
