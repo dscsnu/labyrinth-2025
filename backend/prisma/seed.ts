b968fd279ffee2dbcfa9037620222b60442706ca
lalitm1004 <lalitm1004@gmail.com>
2025-04-10 17:55:12 +0530

    feat: create seed script with placeholder data for Spell and Level table

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -94,9 +94,50 @@ async function createUserTriggers() {
     }
 }
 
+async function seedLevelTable() {
+    const levels = [
+        { name: 'Ex 1', targetScore: 10 },
+        { name: 'Ex 2', targetScore: 20 },
+        { name: 'Ex 3', targetScore: 30 },
+        { name: 'Ex 4', targetScore: 40 },
+        { name: 'Ex 5', targetScore: 50 },
+        { name: 'Ex 6', targetScore: 60 },
+        { name: 'Ex 7', targetScore: 120 },
+    ];
+
+    await prisma.level.createMany({
+        data: levels
+    });
+}
+
+async function seedSpellTable() {
+    const spells = [
+        { rewardScore: 10, numPoints: 2, cooldown: 1 },
+        { rewardScore: 20, numPoints: 3, cooldown: 2 },
+        { rewardScore: 30, numPoints: 3, cooldown: 2 },
+        { rewardScore: 30, numPoints: 3, cooldown: 2 },
+        { rewardScore: 40, numPoints: 4, cooldown: 3 },
+        { rewardScore: 40, numPoints: 4, cooldown: 3 },
+        { rewardScore: 50, numPoints: 4, cooldown: 3 },
+        { rewardScore: 100, numPoints: 8, cooldown: 5 },
+    ];
+
+    await prisma.spell.createMany({
+        data: spells
+    });
+}
+
 async function main() {
     await createUserTriggers()
         .then(() => console.log('✅ userTriggers created'))
+        .catch((e) => console.error(`🚨 ${e}`));
+
+    await seedLevelTable()
+        .then(() => console.log('✅ seeded Level table'))
+        .catch((e) => console.error(`🚨 ${e}`));
+
+    await seedSpellTable()
+        .then(() => console.log('✅ seeded Spell table'))
         .catch((e) => console.error(`🚨 ${e}`));
 }
 
