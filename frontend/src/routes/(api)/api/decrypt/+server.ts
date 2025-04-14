8372e0f639536ad031ddbe2f2d9b1d7588c4cef4
rohitjg13 <jgrohit.exe@gmail.com>
2025-04-15 01:25:06 +0530

    encrypt/decrypt for qr

---
+++ frontend/src/routes/(api)/api/decrypt/+server.ts
@@ -0,0 +1,31 @@
+import { error, json } from '@sveltejs/kit';
+import jwt from 'jsonwebtoken';
+
+export async function POST({ request }) {
+    try {
+        const data = await request.json();
+        if (!data.token) {
+            throw error(400, 'Token is required');
+        }
+
+        const JWT_SESSION_KEY = process.env.JWT_SESSION_KEY;
+
+        const decoded = jwt.verify(data.token, JWT_SESSION_KEY);
+        let valid = false;
+        let validTime = 5; //in seconds
+        if (decoded && decoded.payload && decoded.timestamp) {
+            const currentTime = Math.floor(Date.now() / 1000);
+            if (currentTime < decoded.timestamp + validTime) {
+                valid = true;
+            }
+        }
+
+        return json({
+            valid: valid,
+            payload: decoded.payload,
+        });
+    } catch (err) {
+        console.error('Error decrypting token:', err);
+        return error(500, 'Failed to decrypt token');
+    }
+}
