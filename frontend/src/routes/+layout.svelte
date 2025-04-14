42f0d81403b6bedbcd40d967c9d8f2d7ad849c68
AadishJ <jain.aadishj@gmail.com>
2025-04-14 21:47:48 +0530

    Token FIx

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
-	import '../app.css';
-    import { onMount } from 'svelte';
-    import { invalidateAll } from '$app/navigation';
-    import { device, setDevice } from '$lib/stores/DeviceStore';
-    import { SupaStore, UserStore } from '$lib/stores/SupabaseStore';
-    import MobileOnly from '$lib/components/MobileOnly.svelte';
-    import { PUBLIC_ENVIRONMENT } from '$env/static/public';
-    import { setToken, token } from '$lib/stores/TokenStore';
-    import { ToastStore } from '$lib/stores/ToastStore';
-    import Toast from '$lib/components/Toast.svelte';
-    import { LoadingStore } from '$lib/stores/LoadingStore';
-    import Loading from '$lib/components/Loading.svelte';
+	import "../app.css";
+	import { onMount } from "svelte";
+	import { invalidateAll } from "$app/navigation";
+	import { device, setDevice } from "$lib/stores/DeviceStore";
+	import { SupaStore, UserStore } from "$lib/stores/SupabaseStore";
+	import MobileOnly from "$lib/components/MobileOnly.svelte";
+	import { PUBLIC_ENVIRONMENT } from "$env/static/public";
+	import { setToken } from "$lib/stores/TokenStore";
+	import { ToastStore } from "$lib/stores/ToastStore";
+	import Toast from "$lib/components/Toast.svelte";
+	import { LoadingStore } from "$lib/stores/LoadingStore";
+	import Loading from "$lib/components/Loading.svelte";
 
 	let { data, children } = $props();
 	let { supabase, session, user } = $derived(data);
 
 	onMount(() => {
 		const {
-			data: { subscription }
+			data: { subscription },
 		} = supabase.auth.onAuthStateChange((event, newSession) => {
-			if (newSession?.expires_at !== session?.expires_at || event === 'SIGNED_OUT') {
+			if (
+				newSession?.expires_at !== session?.expires_at ||
+				event === "SIGNED_OUT"
+			) {
 				setToken(null);
 				invalidateAll();
 			}
 
-			if (['SIGNED_IN', 'TOKEN_REFRESHED'].includes(event) && session?.access_token) {
+			if (
+				["SIGNED_IN", "TOKEN_REFRESHED"].includes(event) &&
+				session?.access_token
+			) {
 				setToken(session.access_token);
 			}
 		});
@@ -32,17 +38,25 @@
 		SupaStore.set(supabase);
 		UserStore.set(user);
 
-		if (window.matchMedia('(max-width: 767px)').matches) setDevice('mobile');
-        else setDevice('desktop');
+		if (window.matchMedia("(max-width: 767px)").matches)
+			setDevice("mobile");
+		else setDevice("desktop");
 
 		return () => subscription.unsubscribe();
-	})
+	});
 </script>
 
 <!-- handle device resize -->
-<svelte:window onresize={() => setDevice(window.matchMedia('(max-width: 767px').matches ? 'mobile' : 'desktop')}/>
+<svelte:window
+	onresize={() =>
+		setDevice(
+			window.matchMedia("(max-width: 767px").matches
+				? "mobile"
+				: "desktop",
+		)}
+/>
 
-{#if $device === 'mobile' || PUBLIC_ENVIRONMENT === 'development'}
+{#if $device === "mobile" || PUBLIC_ENVIRONMENT === "development"}
 	{@render children()}
 {:else}
 	<MobileOnly />
@@ -50,7 +64,7 @@
 
 {#if $ToastStore}
 	{#each $ToastStore as toast (toast.id)}
-		<Toast {...toast}/>
+		<Toast {...toast} />
 	{/each}
 {/if}
 
