608affad79637b12494096554ebe105b73931034
Rachit Kumar <rachit.prodigy205@gmail.com>
2025-04-16 02:38:01 +0530

    modify streaming response string

--- backend/internal/controllers/controllers.go
+++ backend/internal/controllers/controllers.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"context"
 	"encoding/json"
 	"labyrinth/internal/controllers/middleware"
 	"labyrinth/internal/protocol"
@@ -26,7 +25,7 @@ func HandleAll(rtr *router.Router) {
 	rtr.HandleFunc("/api/team/update", middleware.Authorized(rtr, Post(TeamUpdateHandler(rtr))))
 	rtr.HandleFunc("/api/team/leave", middleware.Authorized(rtr, Post(LeaveTeamHandler(rtr))))
 
-	rtr.HandleFunc("/api/eventlistener", middleware.Authorized(rtr, TeamChannelEventHandler(rtr)))
+	rtr.HandleFunc("/api/eventlistener", Get(TeamChannelEventHandler(rtr)))
 
 	rtr.Handle("/swagger/", http.StripPrefix("/swagger/", httpSwagger.Handler(
 		httpSwagger.URL("http://localhost:3100/swagger/doc.json"),
@@ -51,32 +50,35 @@ func TeamChannelEventHandler(rtr *router.Router) http.HandlerFunc {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		userEmail := r.Context().Value("email").(string)
+		//userEmail := r.Context().Value("email").(string)
 
-		user, err := rtr.State.DB.GetUser(context.Background(), userEmail)
-		if err != nil {
+		query := r.URL.Query()
+		teamId := query.Get("team_id")
 
-			http.Error(w, "internal error occurred", http.StatusInternalServerError)
-			rtr.Logger.Error("internal error", "error", err)
-			return
-		}
+		//user, err := rtr.State.DB.GetUser(context.Background(), userEmail)
+		//if err != nil {
+		//
+		//	http.Error(w, "internal error occurred", http.StatusInternalServerError)
+		//	rtr.Logger.Error("internal error", "error", err)
+		//	return
+		//}
 
-		team, err := rtr.State.DB.GetTeamByUserId(context.Background(), user.ID)
+		//team, err := rtr.State.DB.GetTeamByUserId(context.Background(), user.ID)
 
-		if err != nil {
+		//if err != nil {
 
-			http.Error(w, "internal error occurred, is the user in a valid team?", http.StatusInternalServerError)
-			return
-		}
+		//	http.Error(w, "internal error occurred, is the user in a valid team?", http.StatusInternalServerError)
+		//	return
+		//}
 
-		teamChannel := rtr.State.ChanPool.GetChannel(team.ID)
+		teamChannel := rtr.State.ChanPool.GetChannel(teamId)
 		listenerChannel := make(chan protocol.Packet)
 		teamChannel.AddMember(listenerChannel)
 
 		//w.Header().Add("Content-Type", "text/event-stream")
-		w.Header().Add("Content-Type", "text/event-stream")
-		w.Header().Add("Cache-Control", "no-cache")
-		w.Header().Add("Connection", "keep-alive")
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
 		w.WriteHeader(http.StatusOK)
 
 		flusher, ok := w.(http.Flusher)
@@ -87,13 +89,35 @@ func TeamChannelEventHandler(rtr *router.Router) http.HandlerFunc {
 
 		}
 
+		//for eventMessage := range listenerChannel {
+
+		//	if err := json.NewEncoder(w).Encode(eventMessage); err != nil {
+
+		//		rtr.Logger.Debug("http stream write failed")
+
+		//	}
+		//	flusher.Flush()
+		//}
+
 		for eventMessage := range listenerChannel {
-
-			if err = json.NewEncoder(w).Encode(eventMessage); err != nil {
-
-				rtr.Logger.Debug("http stream write failed")
-
+			// Convert event message to JSON string
+			data, err := json.Marshal(eventMessage)
+			if err != nil {
+				rtr.Logger.Debug("failed to marshal event message", slog.String("error", err.Error()))
+				continue
 			}
+
+			// Construct SSE message
+			sseMessage := "data: " + string(data) + "\n\n"
+
+			// Write the SSE message to the response
+			_, err = w.Write([]byte(sseMessage))
+			if err != nil {
+				rtr.Logger.Debug("http stream write failed", slog.String("error", err.Error()))
+				return
+			}
+
+			// Flush the output to ensure immediate delivery of the message
 			flusher.Flush()
 		}
 
