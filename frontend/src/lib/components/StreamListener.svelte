665e8104bdb79b3251b07cbdef86f3a3e8d399d0
lalitm1004 <lalitm1004@gmail.com>
2025-04-15 22:13:47 +0530

    feat: skeleton stream listener

---
+++ frontend/src/lib/components/StreamListener.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { PUBLIC_BACKEND_URL, PUBLIC_STREAM_URL } from "$env/static/public";
+    import { JwtTokenStore } from "$lib/stores/JwtTokenStore";
+    import { TeamStore } from "$lib/stores/TeamStore";
+    import { EventSourcePolyfill } from "event-source-polyfill";
+    import { onMount } from "svelte";
+    import { get } from "svelte/store";
+
+    let eventSource: EventSourcePolyfill | null = $state(null);
+
+    const cleanup = () => {
+        if (eventSource) {
+            eventSource.close();
+            eventSource = null;
+        }
+    }
+
+    const connect = () => {
+        console.log('Attempting Connection');
+        const cleanedUrl = PUBLIC_BACKEND_URL.replace(/\/+$/, '');
+        const cleanedEndpoint = PUBLIC_STREAM_URL.replace(/^\/+/, '');
+        const jwt = get(JwtTokenStore);
+
+        eventSource = new EventSourcePolyfill(`${cleanedUrl}/${cleanedEndpoint}`, {
+            headers: {
+                'Authorization': `Bearer ${jwt}`
+            }
+        });
+
+        eventSource.onmessage = (event) => {
+            alert(event.data);
+        }
+
+        eventSource.onerror = (error) => {
+            console.error(`Stream error: ${error}`);
+            cleanup();
+        }
+    }
+
+    onMount(() => {
+        const unsubscribe = TeamStore.subscribe(current => {
+            if (current) {
+                connect();
+            } else {
+                cleanup();
+            }
+        })
+
+        return () => unsubscribe();
+    })
+</script>
