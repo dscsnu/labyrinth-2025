c252e5606ec984b1e81d16302d5915102765c00d
rohitjg13 <jgrohit.exe@gmail.com>
2025-04-17 10:58:22 +0530

    Updated PostgresDriver Structure to include cache support (And required functions)

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"crypto/rand"
 	"math/big"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/patrickmn/go-cache"
 )
 
 func UUID(v uuid.UUID) pgtype.UUID {
@@ -30,7 +32,8 @@ func genRand() (string, error) {
 }
 
 type PostgresDriver struct {
-	pool *pgxpool.Pool
+	pool  *pgxpool.Pool
+	cache *cache.Cache
 }
 
 func (pd *PostgresDriver) Close() {
@@ -42,5 +45,31 @@ func CreatePostgresDriver(connectionURL string) (*PostgresDriver, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &PostgresDriver{pool: pool}, nil
+
+	c := cache.New(5*time.Minute, 10*time.Minute)
+
+	return &PostgresDriver{
+		pool:  pool,
+		cache: c,
+	}, nil
+}
+
+func (pd *PostgresDriver) SetCacheTTL(defaultExpiration, cleanupInterval time.Duration) {
+	pd.cache = cache.New(defaultExpiration, cleanupInterval)
+}
+
+func (pd *PostgresDriver) CacheSet(key string, value interface{}, expiration time.Duration) {
+	pd.cache.Set(key, value, expiration)
+}
+
+func (pd *PostgresDriver) CacheGet(key string) (interface{}, bool) {
+	return pd.cache.Get(key)
+}
+
+func (pd *PostgresDriver) CacheDelete(key string) {
+	pd.cache.Delete(key)
+}
+
+func (pd *PostgresDriver) CacheFlush() {
+	pd.cache.Flush()
 }
