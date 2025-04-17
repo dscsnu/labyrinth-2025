698ed73cf03a72c1bc6a8a3fe0137a0bf52efc0a
lalitm1004 <lalitm1004@gmail.com>
2025-04-17 15:11:56 +0530

    fix: default to mobile

--- frontend/src/lib/stores/DeviceStore.ts
+++ frontend/src/lib/stores/DeviceStore.ts
@@ -2,7 +2,7 @@ import { browser } from "$app/environment";
 import { writable } from "svelte/store";
 
 const TOKEN_NAME = 'labyrinth-gdsc-device'
-const initialValue = browser ? window.localStorage.getItem(TOKEN_NAME) ?? 'desktop' : 'desktop';
+const initialValue = browser ? window.localStorage.getItem(TOKEN_NAME) ?? 'mobile' : 'mobile';
 const device = writable<Device>(initialValue as Device);
 const setDevice = (value: Device) => {
     if (!browser) return;
